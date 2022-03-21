6f470d9c1ceeebbcaee9390ebd3f6d1c9b3489fe
Marcos Cunha <marcos.cunha@ffit.com.br>
2022-03-21 16:58:22 -0300

    Create Template

--- portfolio/tasks/views.py
+++ portfolio/tasks/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('Bem vindo(a) Dev')
+def homepage(request):
+    return render(request, 'tasks/index.html')
